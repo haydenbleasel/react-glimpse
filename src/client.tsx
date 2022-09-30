6f1700861e9435aecd255f5fc99c49e4f8ba7eac
Hayden Bleasel <hello@haydenbleasel.com>
2022-09-29 20:15:56 -0400

    Remove unused placeholder component

--- src/client.tsx
+++ src/client.tsx
@@ -3,37 +3,6 @@ import { Root } from '@radix-ui/react-portal';
 import type { FC, ReactNode, RefObject } from 'react';
 import { useContext, createContext } from 'react';
 
-const LoadingIcon: FC = () => (
-  <svg
-    className="h-5 w-5 animate-spin text-neutral-400"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-  >
-    <circle
-      className="opacity-25"
-      cx="12"
-      cy="12"
-      r="10"
-      stroke="currentColor"
-      strokeWidth="4"
-    />
-    <path
-      className="opacity-75"
-      fill="currentColor"
-      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-    />
-  </svg>
-);
-
-const Placeholder: FC<{ className?: string }> = ({ className = '' }) => (
-  <span
-    className={`flex items-center justify-center bg-neutral-50 dark:bg-neutral-800 ${className}`}
-  >
-    <LoadingIcon />
-  </span>
-);
-
 type LinkPreviewProps = {
   image?: string | null;
   title?: string | null;
@@ -100,17 +69,17 @@ const Image: FC<{
 }) => {
   const { url, image } = useContext(LinkPreviewContext);
 
+  if (!image) {
+    return null;
+  }
+
   return (
     <div style={{ height }}>
-      {image ? (
-        <img
-          src={image}
-          alt={`Preview of ${url ?? 'a website'}`}
-          className={className}
-        />
-      ) : (
-        <Placeholder className="h-full w-full rounded-sm" />
-      )}
+      <img
+        src={image}
+        alt={`Preview of ${url ?? 'a website'}`}
+        className={className}
+      />
     </div>
   );
 };
